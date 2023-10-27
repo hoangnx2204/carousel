d13056e11f7bf288c714b5f74c480a919cc694da
HOANG XUAN NGUYEN/LGEDV webOS TEAM(hoang10.nguyen@lge.com) <hoang10.nguyen@lge.com>
2023-10-27 15:28:42 +0700

    shift first item list

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,7 +27,10 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            const AnimatedCarousel(),
+            const SizedBox(
+              height: 60,
+              child: AnimatedCarousel(),
+            )
           ],
         ),
       ),
@@ -37,17 +41,33 @@ class _HomePageState extends State<HomePage> {
 class AnimatedCarousel extends StatefulWidget {
   const AnimatedCarousel({
     super.key,
-    this.height = 200,
   });
-  final double height;
 
   @override
   State<AnimatedCarousel> createState() => _AnimatedCarouselState();
 }
 
-class _AnimatedCarouselState extends State<AnimatedCarousel> {
+class _AnimatedCarouselState extends State<AnimatedCarousel>
+    with SingleTickerProviderStateMixin {
   final itemCount = 20;
-  late final focusNodes = List.generate(itemCount, (index) => FocusNode());
+  int? focusedIndex;
+  late final animController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+  late final scaleAnim = Tween<double>(begin: 1, end: 1.2).animate(
+    animController,
+  );
+  final List<FocusNode> focusNodes = List.generate(
+    30,
+    (index) => FocusNode(debugLabel: index.toString()),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   void dispose() {
     for (var node in focusNodes) {
@@ -58,136 +78,95 @@ class _AnimatedCarouselState extends State<AnimatedCarousel> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView.separated(
-        itemCount: 20,
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        addAutomaticKeepAlives: true,
-        itemBuilder: (context, index) {
-          final focusNode = focusNodes[index];
-          return CarouselItem(
-            focusNode: focusNode,
-            lable: index.toString(),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(width: 10);
-        },
-      ),
+    return ListView.separated(
+      itemCount: 30,
+      cacheExtent: MediaQuery.of(context).size.width,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        return Shortcuts(
+          shortcuts: {
+            const SingleActivator(LogicalKeyboardKey.arrowLeft):
+                CarouselMoveIntent(
+              index > 0 ? focusNodes.elementAtOrNull(index - 1) : null,
+            ),
+            const SingleActivator(LogicalKeyboardKey.arrowRight):
+                CarouselMoveIntent(
+              index <= focusNodes.length - 1
+                  ? focusNodes.elementAtOrNull(index + 1)
+                  : null,
+            ),
+          },
+          child: CarouselItem(
+            focusNode: focusNodes[index],
+            title: index.toString(),
+            isSelected: focusedIndex == index,
+            onFocusChange: (focused) {
+              if (focused) {
+                // print('AppLog: ${focusNodes[index].debugLabel}');
+              }
+            },
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(width: 10);
+      },
     );
   }
 }
 
-class CarouselItem extends StatefulWidget {
+class CarouselItem extends StatelessWidget {
   const CarouselItem({
     super.key,
+    required this.title,
+    this.isSelected = false,
+    this.onFocusChange,
+    this.onTap,
     this.focusNode,
-    this.lable = '',
   });
-
+  final String title;
+  final bool isSelected;
+  final ValueChanged<bool>? onFocusChange;
+  final ValueChanged<double>? onTap;
   final FocusNode? focusNode;
-  final String lable;
-
-  @override
-  State<CarouselItem> createState() => _CarouselItemState();
-}
-
-class _CarouselItemState extends State<CarouselItem>
-    with SingleTickerProviderStateMixin {
-  bool selected = false;
-  late final animationController = AnimationController(
-    vsync: this,
-    duration: const Duration(milliseconds: 200),
-  );
-  late final scaleAnim =
-      Tween<double>(begin: 1, end: 1.2).animate(animationController);
-  final key = GlobalKey();
-  Offset overlayPosition = Offset.zero;
-  OverlayEntry get overlayEntry => OverlayEntry(
-        builder: (context) {
-          return Material(
-            color: Colors.transparent,
-            child: Stack(
-              children: [
-                Positioned(
-                  top: overlayPosition.dy,
-                  left: overlayPosition.dx,
-                  child: ScaleTransition(
-                    scale: scaleAnim,
-                    child: RealItem(
-                      selected: selected,
-                      label: widget.lable,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      );
-  @override
-  void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final renderBox = context.findRenderObject() as RenderBox?;
-      if (renderBox != null) {
-        overlayPosition = renderBox.localToGlobal(Offset.zero);
-      }
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Focus(
-      focusNode: widget.focusNode,
-      onFocusChange: (isFocused) {
-        setState(() {
-          selected = isFocused;
-        });
-        if (selected) {
-          Overlay.of(context).insert(overlayEntry);
-          animationController.forward();
-        } else {
-          animationController.reverse();
-          // overlayEntry.remove();
-        }
+    return Actions(
+      actions: {
+        CarouselMoveIntent: CallbackAction(
+          onInvoke: (intent) {
+            final focusNode = (intent as CarouselMoveIntent).nextNode;
+            if (focusNode == null) {
+              print('AppLog: do nothing');
+              return;
+            }
+            Scrollable.ensureVisible(focusNode.context ?? context,
+                duration: const Duration(milliseconds: 200));
+            focusNode.requestFocus();
+            return;
+          },
+        )
       },
-      child: RealItem(
-        key: key,
-        selected: selected,
-        label: widget.lable,
+      child: Focus(
+        focusNode: focusNode,
+        onFocusChange: onFocusChange,
+        child: Container(
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(),
+            color: isSelected ? Colors.amber : Colors.white,
+          ),
+          child: Center(child: Text(title)),
+        ),
       ),
     );
   }
 }
 
-class RealItem extends StatelessWidget {
-  const RealItem({
-    super.key,
-    required this.selected,
-    this.label = '',
-  });
+class CarouselMoveIntent extends Intent {
+  final FocusNode? nextNode;
 
-  final bool selected;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        color: selected ? Colors.teal : Colors.white,
-        border: Border.all(),
-      ),
-      child: Text(label),
-    );
-  }
+  const CarouselMoveIntent(this.nextNode);
 }
